4c2d68649674d8c8abcc902fab1d754e8e859b05
David.OH <dev_david@naver.com>
2018-02-26 22:46:38 +0900

    #3 메인 액티비티 화면 구성
    1. BottomNavigationView 생성
    2. Fragment 생성

--- app/src/main/java/com/plplim/david/calendar/activity/LoginActivity.java
+++ app/src/main/java/com/plplim/david/calendar/activity/LoginActivity.java
@@ -2,8 +2,9 @@ package com.plplim.david.calendar.activity;
 
 import android.content.Intent;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
--- app/src/main/java/com/plplim/david/calendar/activity/MainActivity.java
+++ app/src/main/java/com/plplim/david/calendar/activity/MainActivity.java
@@ -1,10 +1,17 @@
 package com.plplim.david.calendar.activity;
 
+import android.app.Fragment;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.plplim.david.calendar.R;
+import com.plplim.david.calendar.fragment.AccountFragment;
+import com.plplim.david.calendar.fragment.AddFragment;
+import com.plplim.david.calendar.fragment.TodoFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +20,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.mainactivity_bottomnavigationview);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_todo:
+                        getFragmentManager().beginTransaction().replace(R.id.mainactivity_framelayout, new TodoFragment()).commit();
+                        return true;
+
+                    case R.id.action_add:
+                        getFragmentManager().beginTransaction().replace(R.id.mainactivity_framelayout, new AddFragment()).commit();
+                        return true;
+
+                    case R.id.action_account:
+                        getFragmentManager().beginTransaction().replace(R.id.mainactivity_framelayout, new AccountFragment()).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
